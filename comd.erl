538a2510b60f6731fbf6d52d32c133addc739f19
Allen McPherson <al_web_mail@lanl.gov>
2013-05-03 15:56:18 -0600

    Made receive loop to see if we are getting other messages.

--- comd.erl
+++ comd.erl
@@ -10,23 +10,28 @@ string_to_num(S) ->
      	F
     end.
 
+loop(Port) ->
+    receive
+        {Port, {data, Data}} ->
+            io:format("Received data: ~w~n", [Data]),
+            L = string:tokens(Data, " "),
+            io:format("L: ~p~n", [L]),
+            Fun = fun string_to_num/1,
+            lists:foreach(Fun, L),
+            io:format("Data: ~p~n", [Data]);
+        Other ->
+            io:format("Unexpected data: ~p~n", [Other])
+
+    end,
+    loop(Port).
+
 p() ->
     io:format("Starting~n", []),
     Cmd = "./comd -x 123.0 -y 234.0 -z 345.0",   % don't need newline
+    process_flag(trap_exit, true),
     Port = open_port({spawn,Cmd}, [use_stdio, exit_status]),
     Payload = list_to_binary("1.0 2.0 3.0\n") ,
     io:format("Opened the port: ~w~n", [Port]),
     erlang:port_command(Port, Payload),
     io:format("Sent command to port: ~p~n", [Payload]),
-    receive
-        {Port, {data, Data}} ->
-            io:format("Received data: ~w~n", [Data]),
-        	L = string:tokens(Data, " "),
-        	io:format("L: ~p~n", [L]),
-        	Fun = fun string_to_num/1,
-        	lists:foreach(Fun, L),
-            io:format("Data: ~p~n", [Data]);
-        Other ->
-            io:format("Unexpected data: ~p~n", [Other])
-
-    end.
+    loop(Port).
