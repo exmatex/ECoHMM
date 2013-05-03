61f23cbb93b78245a83bd1d4baa329def9c94aa0
Allen McPherson <al_web_mail@lanl.gov>
2013-05-03 15:07:57 -0600

    Command line argument processing.

--- comd.c
+++ comd.c
@@ -1,9 +1,32 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-	float   x, y, z;
+	int     option = 0;
+	float   x = 0.0, 
+			y = 0.0, 
+			z = 0.0;
 
-	scanf("%f %f %f", &x, &y, &z);
+    while ((option = getopt(argc, argv,"x:y:z:")) != -1) {
+        switch (option) {
+            case 'x' : 
+             	x = strtod(optarg, NULL); 
+                break;
+            case 'y' : 
+             	y = strtod(optarg, NULL); 
+                break;
+            case 'z' : 
+             	z = strtod(optarg, NULL); 
+                break;
+             default: 
+                exit(EXIT_FAILURE);
+        }
+    }
+
+
+	//scanf("%f %f %f", &x, &y, &z);
 	printf("%f %f %f\n", x, y, z);
 }
