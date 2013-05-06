cad04588848cb96eff873598ed15828a0edfe935
The Geezer <allen.mcpherson@gmail.com>
2013-05-06 15:16:40 -0600

    Dynamically starting a variable number of serveres.

--- comd_sup.erl
+++ comd_sup.erl
@@ -2,45 +2,45 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/1]).
+-export([start_link/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
--define(COMMAND_LINE, "./comd -x 123.0 -y 234.0 -z 345.0").
+-define(CMD, "./comd -x 123.0 -y 234.0 -z 345.0").
+
 
 %%====================================================================
 %% API
 %%====================================================================
 
-start_link(Pid) ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, [Pid]).
+start_link(Pid, N) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [Pid, N]).
 
 %%====================================================================
 %% Supervisor callbacks
 %%====================================================================
 
-init([Pid]) ->
-    % Need to dynaically add children with names composed of
-    % comd and the cell number of the server.
-    Butt =  % Starting one server fo now.
-        {tag1,
-         {comd_srv, start_link, [comd_0, ?COMMAND_LINE, 0, 0, Pid]},
-         transient, 2000, worker, dynamic},
-    Nutt =  % Starting one server fo now.
-        {tag2,
-         {comd_srv, start_link, [comd_1, ?COMMAND_LINE, 1, 0, Pid]},
-         transient, 2000, worker, dynamic},
-    Futt =  % Starting one server fo now.
-        {tag3,
-         {comd_srv, start_link, [comd_2, ?COMMAND_LINE, 2, 0, Pid]},
-         transient, 2000, worker, dynamic},
-
-    Children = [Butt, Nutt, Futt],
-    {ok, {{one_for_one, 3, 10}, Children}}.
+init([Pid, N]) ->
+    L = lists:seq(0, N-1, 1),
+    Children = lists:map(
+        fun(I) ->
+            {mk_name("tag_", I),
+             {comd_srv, start_link, [mk_name("comd_", I), ?CMD, I, 0, Pid]},
+             transient, 2000, worker, dynamic}
+        end,
+        L
+    ),
+    io:format("~p~n", [Children]),
+    {ok, { {one_for_one, 3, 10}, Children }}.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+mk_name(Base, N) ->
+    list_to_atom(Base ++ integer_to_list(N)).
+
+
 
--- hmm.erl
+++ hmm.erl
@@ -3,16 +3,24 @@
 -export([run/1]).
 
 run(N) ->
-	{ok, Pid} = comd_sup:start_link(self()),
+	{ok, Pid} = comd_sup:start_link(self(), N),
 	unlink(Pid),
-	loop(N).
+	AllNames = get_all_names(N),
+	run_app(100).
 
-loop(N) when N > 0 ->
+get_all_names(N) when N > 0 ->
 	receive
 		{comd_started, {Cell, Name}} ->
 			io:format("comd_started: [~p]~p~n", [Cell, Name])
 	end,
-	loop(N-1);
-loop(0) ->
+	get_all_names(N-1);
+get_all_names(0) ->
 	ok.
 	
+run_app(TS) when TS > 0 ->
+	%send_stress(AllNames),
+	%get_strain(AllNames),
+	%update_stress(AllNames),
+	run_app(TS-1);
+run_app(0) ->
+	ok.
